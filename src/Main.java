beeb563b61b3f2a67c8715fff9d686989f40fe93
AndreyChirkin <106620486+AndrewChirkin@users.noreply.github.com>
2022-08-25 23:49:53 +0300

    hw2Part2Task1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+    int vegetables = 8;
+    System.out.println("Значение переменной vegetables с типом int равно " +  vegetables);
+    byte fruits = 11;
+    System.out.println("Значение переменной fruits с типом byte равно " + fruits);
+    short berries = 6;
+    System.out.println("Значение переменной berries с типом short равно " + berries);
+    long years = 666L;
+    System.out.println("Значение переменной years с типом long равно " + years);
+    float pi = (float) 3.1415926535897932384626433832795;
+    System.out.println("Значение переменной pi с типом float равно " + pi);
+    double cappuccino = 2;
+    System.out.println("Значение переменной cappuccino с типом double равно " + cappuccino);
+    }
+}
