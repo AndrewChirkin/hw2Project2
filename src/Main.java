25fcb4e69bf8c2652cf157667b278bc0163a0ecd
AndreyChirkin <106620486+AndrewChirkin@users.noreply.github.com>
2022-08-26 20:11:13 +0300

    hw2Part2Task3

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-    int vegetables = 27897;
-    System.out.println("Значение переменной vegetables с типом int равно " +  vegetables);
-    byte fruits = 67;
-    System.out.println("Значение переменной fruits с типом byte равно " + fruits);
-    short dollars = -159;
-    System.out.println("Значение переменной dollars с типом short равно " + dollars);
-    long years = 987678965549L;
-    System.out.println("Значение переменной years с типом long равно " + years);
-    float pi = (float) 27.12;
-    System.out.println("Значение переменной pi с типом float равно " + pi);
-    double smth = 2.7876;
-    System.out.println("Значение переменной smth с типом double равно " + smth);
-    boolean truth = false;
-    System.out.println("Значение переменной truth с типом boolean равно " + truth);
-    char chairs = 569;
-    System.out.println("Значение переменной chairs с типом char равно " + chairs);
+    byte studentsLP = 23;
+    byte studentsAS = 27;
+    byte studentsEA = 30;
+    short totalPaper = 480;
+    int paperForEverybody = totalPaper / (studentsAS + studentsEA + studentsLP);
+    System.out.println("На каждого ученика рассчитано " + paperForEverybody + " листов бумаги");
     }
 }
